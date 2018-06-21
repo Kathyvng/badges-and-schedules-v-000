95bbea1029bc6866ad6d6f0c0bb22adcb2bdef38
Kathy Nguyen <kathyvng@gmail.com>
2018-06-21 04:54:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,10 @@
-def conference_badges(name, attendees, badges)
+def conference_badges(name, attendees, badges, room_assignments)
   name = "Arel"
   attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   badges = "Hello, my name is #{attendees}."
+  room_assignments.each do |number|
+    number = 0
+    puts "Hello, #{attendees}! You'll be assigned to room #{number}!"
+    number += 1
+  end 
 end
